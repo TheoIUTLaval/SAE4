2cfbbe28d83be05211a9c5040ee45ab246a92dfa
mathilde2801 <mathilde72m@gmail.com>
2025-03-18 16:46:38 +0100

    Remove unused popup styles from template.css and link to new popups.css in template.php

--- template.php
+++ template.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/template.css">
+    <link rel="stylesheet" type="text/css" href="css/popups.css">
+    
 </head>
 <body>
     <?php
