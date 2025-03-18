c587ba3f4d1fb888cd3ab2f871ea07f75e91cf4b
mathilde2801 <mathilde72m@gmail.com>
2025-03-18 16:53:05 +0100

    No code changes made.

--- template.php
+++ template.php
@@ -3,7 +3,7 @@
 <head>
     <title>L'étal en ligne</title>
     <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
     <link rel="stylesheet" type="text/css" href="css/template.css">
 </head>
 <body>
@@ -69,5 +69,4 @@
         </div>
     </div>
     <?php require "popups/gestion_popups.php";?>
-    
 </body>
