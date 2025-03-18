f6b79891aecd5fe614b680bc0b4d938d2e3ff37c
mathilde2801 <mathilde72m@gmail.com>
2025-03-18 16:56:44 +0100

    Rename CSS class from 'container' to 'truc' in template.php and template.css; remove unused popups.css link

--- template.php
+++ template.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style_general.css">
-    <link rel="stylesheet" type="text/css" href="css/popups.css">
     
 </head>
 <body>
@@ -14,7 +13,7 @@
             session_start();
         }
     ?>
-    <div class="container">
+    <div class="truc">
         <div class="leftColumn">
 			<img class="logo" href="index.php" src="asset/img/logo.png" alt="Logo">
             <div class="contenuBarre">
