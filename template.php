4ab312d7256f5bc9e2328e076c1af3194cf8b582
mathilde2801 <mathilde72m@gmail.com>
2025-03-18 16:22:24 +0100

    Update stylesheet path in template.php for correct loading

--- template.php
+++ template.php
@@ -4,7 +4,7 @@
     <title>L'étal en ligne</title>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="templates.css">
+    <link rel="stylesheet" href="css/templates.css">
 </head>
 <body>
     <?php
