1c6add7f7782c2a8f7e4e7b8fe01c6a142dae4a8
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-19 14:52:59 +0100

    Update controllerIndex.php

--- controller/controllerIndex.php
+++ controller/controllerIndex.php
@@ -35,36 +35,6 @@ if (isset($_SESSION["language"])==false){
     $_SESSION["language"]="fr";
 }
 
-function latLongGps($url){
-    // Configuration de la requête cURL
-    $ch = curl_init($url);
-    // Si vous avez besoin d'un proxy, décommentez les lignes suivantes et ajustez les paramètres
-    curl_setopt($ch, CURLOPT_PROXY, 'proxy.univ-lemans.fr');
-    curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-    curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    // Exécution de la requête
-    $response = curl_exec($ch);
-    // Vérifier s'il y a eu une erreur cURL
-    if (curl_errno($ch)) {
-        echo 'Erreur cURL : ' . curl_error($ch);
-        curl_close($ch);
-        return [0, 0];
-    } else {
-        // Analyser la réponse JSON
-        $data = json_decode($response);
-        // Vérifier si la réponse a été correctement analysée
-        if (!empty($data) && is_array($data) && isset($data[0])) {
-            // Récupérer la latitude et la longitude
-            $latitude = $data[0]->lat ?? 0;
-            $longitude = $data[0]->lon ?? 0;
-            curl_close($ch);
-            return [$latitude, $longitude];
-        }
-        curl_close($ch);
-        return [0,0];
-    }
-}
 
 /*---------------------------------------------------------------*/
 /*
