252cad1c2b3ab419738a30eb615afe750c236a9d
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-19 14:22:52 +0100

    changement

--- controller/controllerIndex.php
+++ controller/controllerIndex.php
@@ -39,17 +39,11 @@ function latLongGps($url){
     // Configuration de la requête cURL
     $ch = curl_init($url);
     // Si vous avez besoin d'un proxy, décommentez les lignes suivantes et ajustez les paramètres
-    // curl_setopt($ch, CURLOPT_PROXY, 'proxy.univ-lemans.fr');
-    // curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-    // curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+    curl_setopt($ch, CURLOPT_PROXY, 'proxy.univ-lemans.fr');
+    curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+    curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Permet de suivre les redirections
-    // Ajout du User Agent
-    $customUserAgent = "LEtalEnLigne/1.0"; // Remplacez par le nom et la version de votre application
-    curl_setopt($ch, CURLOPT_USERAGENT, $customUserAgent);
-    // Ajout du Referrer
-    $customReferrer = "https://proxy.univ-lemans.fr:3128"; // Remplacez par l'URL de votre application
-    curl_setopt($ch, CURLOPT_REFERER, $customReferrer);
     // Options de débogage
     curl_setopt($ch, CURLOPT_VERBOSE, true);
     curl_setopt($ch, CURLOPT_HEADER, true);
@@ -58,6 +52,8 @@ function latLongGps($url){
     // Vérifier s'il y a eu une erreur cURL
     if (curl_errno($ch)) {
         echo 'Erreur cURL : ' . curl_error($ch);
+        curl_close($ch);
+        return [0, 0];
     } else {
         // Analyser la réponse JSON
         $data = json_decode($response);
@@ -66,12 +62,12 @@ function latLongGps($url){
             // Récupérer la latitude et la longitude
             $latitude = $data[0]->lat ?? 0;
             $longitude = $data[0]->lon ?? 0;
+            curl_close($ch);
             return [$latitude, $longitude];
         }
+        curl_close($ch);
         return [0,0];
     }
-    // Fermeture de la session cURL
-    curl_close($ch);
 }
 
 /*---------------------------------------------------------------*/
