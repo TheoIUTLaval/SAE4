a814b0768259f13e618d47fd592f912487bebd12
mathilde2801 <mathilde72m@gmail.com>
2025-03-19 10:45:34 +0100

    Refactor button hover styles and update button class for consistency

--- template.php
+++ template.php
@@ -108,7 +108,7 @@
                             <input type="hidden" name="popup" value="contact_admin">
                     </button>
                     </form>
-                    <button class="btn btn-outline-primary" type="button" style ="border: 1px solid #305514; border-radius: 5px; padding: 5px; color: #305514"> data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    <button class="btn btn-outline-primary bouton" type="button" style ="border: 1px solid #305514; border-radius: 5px; padding: 5px; color: #305514" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                         Conditions générales d'utilisation
                     </button>
                 
