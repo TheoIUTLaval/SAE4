3a6fc81f3c305a4b29a2b368b256fcd69387d070
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-20 16:35:56 +0100

    ?

--- ViewDelivery.php
+++ ViewDelivery.php
@@ -1,152 +1,74 @@
+<?php
+session_start();
+require "language/language.php";
+require "modele/modeleDelivery.php";
+
+$bdd = dbConnect();
+
+// Vérifier si l'utilisateur est connecté et récupérer son ID
+if (!isset($_SESSION["Id_Uti"])) {
+    die("Utilisateur non connecté !");
+}
+$utilisateur = $_SESSION["Id_Uti"];
+
+// Vérifier si le filtre est défini, sinon prendre 0 (toutes les commandes)
+$filtreCategorie = $_POST['typeCategorie'] ?? 0;
+
+// Récupérer les commandes de l'utilisateur
+$returnQueryGetCommande = getCommandes($bdd, $utilisateur, $filtreCategorie);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <?php
-        session_start();
-        require "language/language.php";
-        include 'modele/modeleDelivery.php';
-    ?>
     <title><?php echo $htmlMarque; ?></title>
     <meta charset="UTF-8">
-    
-    <link rel="stylesheet" type="text/css" href="css/style_général.css">
-    <link rel="stylesheet" type="text/css" href="css/popup.css">
-    <link rel="stylesheet" type="text/css" href="css/template.css">
+    <link rel="stylesheet" href="css/style_général.css">
+    <link rel="stylesheet" href="css/template.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<div class="contenuPage">
+    <?php if (empty($returnQueryGetCommande)): ?>
+        <p><?php echo $htmlAucuneCommande; ?></p>
+    <?php else: ?>
+        <?php foreach ($returnQueryGetCommande as $commande): ?>
+            <div class='commande'>
+                <strong><?php echo $htmlClient; ?>:</strong> <?php echo "{$commande['Prenom_Uti']} {$commande['Nom_Uti']}"; ?><br>
+                <strong><?php echo $htmlCOMMANDE; ?>:</strong> <?php echo strtoupper($commande['Desc_Statut']); ?><br>
 
-    <div class="custom-container">
-        <div class="leftColumn">
-            <a href="index.php"><img class="logo" src="asset/img/logo.png"></a>
-            <div class="contenuBarre">
-                <center><strong><?php echo $htmlFiltrerParDeuxPoints; ?></strong></center>
-                <br>
-                <strong><?php echo $htmlStatut; ?></strong>
-                <br>
-                <form action="ViewDelivery.php" method="post">
-                    <?php
-                    $categories = [
-                        "0" => $htmlTOUT,
-                        "1" => $htmlENCOURS,
-                        "2" => $htmlPRETE,
-                        "3" => $htmlANNULEE,
-                        "4" => $htmlLIVREE
-                    ];
-                    foreach ($categories as $key => $label) {
-                        echo "<label>
-                                <input type='radio' name='typeCategorie' value='$key' " . ($filtreCategorie == $key ? 'checked' : '') . "> $label
-                              </label><br>";
-                    }
-                    ?>
-                    <br>
-                    <center><input type="submit" value="<?php echo $htmlFiltrer; ?>"></center>
-                </form>
-            </div>
-        </div>
-
-        <div class="rightColumn">
-            <div class="topBanner">
-                <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="index.php">Accueil</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto">
-                                <?php if (isset($_SESSION["Id_Uti"])): ?>
-                                    <li class="nav-item"><a class="nav-link" href="ViewMessagerie.php">Messagerie</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="ViewAchats.php">Achats</a></li>
-                                <?php endif; ?>
-                                <?php if (!empty($_SESSION["isProd"])): ?>
-                                    <li class="nav-item"><a class="nav-link" href="produits.php">Produits</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="ViewDelivery.php">Commandes</a></li>
-                                <?php endif; ?>
-                                <?php if (!empty($_SESSION["isAdmin"])): ?>
-                                    <li class="nav-item"><a class="nav-link" href="ViewPanelAdmin.php">Panel Admin</a></li>
-                                <?php endif; ?>
-                                <li class="nav-item">
-                                    <form action="language/language.php" method="post" id="languageForm">
-                                        <select name="language" id="languagePicker" onchange="document.getElementById('languageForm').submit()" class="form-select">
-                                            <option disabled selected>Language</option>
-                                            <?php
-                                            $languages = ["fr" => "Français", "en" => "English", "es" => "Español", "al" => "Deutsch", "ru" => "русский", "ch" => "中國人"];
-                                            foreach ($languages as $code => $lang) {
-                                                echo "<option value='$code' " . ($_SESSION["language"] == $code ? "selected" : "") . ">$lang</option>";
-                                            }
-                                            ?>
-                                        </select>
-                                    </form>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div class="divConnection">
-                    <form method="post">
-                        <?php
-                        $_SESSION['tempPopup'] = $_SESSION['tempPopup'] ?? "";
-                        if ($_SESSION['tempPopup']) {
-                            $_POST['popup'] = $_SESSION['tempPopup'];
-                            unset($_SESSION['tempPopup']);
-                        }
-                        ?>
-                        <input type="submit" value="<?php echo isset($_SESSION['Mail_Uti']) ? $_SESSION['Mail_Uti'] : "Se connecter"; ?>" class="boutonDeConnection">
-                        <input type="hidden" name="popup" value="<?php echo isset($_SESSION['Mail_Uti']) ? 'info_perso' : 'sign_in'; ?>">
+                <!-- Modification du statut de commande -->
+                <?php if (!in_array($commande['Id_Statut'], [3, 4])): ?>
+                    <form action="change_status_commande.php" method="post">
+                        <select name="categorie">
+                            <option value=""><?php echo $htmlModifierStatut; ?></option>
+                            <option value="1"><?php echo $htmlENCOURS; ?></option>
+                            <option value="2"><?php echo $htmlPRETE; ?></option>
+                            <option value="3"><?php echo $htmlANNULEE; ?></option>
+                            <option value="4"><?php echo $htmlLIVREE; ?></option>
+                        </select>
+                        <input type="hidden" name="idCommande" value="<?php echo $commande['Id_Commande']; ?>">
+                        <button type="submit"><?php echo $htmlConfirmer; ?></button>
                     </form>
-                </div>
-            </div>
-            
-            <div class="contenuPage">
+                <?php endif; ?>
+
+                <!-- Affichage des produits associés à la commande -->
                 <?php
-                if (empty($returnQueryGetCommande)) {
-                    echo "<p>$htmlAucuneCommande</p>";
-                } else {
-                    foreach ($returnQueryGetCommande as $commande) {
-                        $Desc_Statut = mb_strtoupper($commande["Desc_Statut"]);
-                        $Nom_Client = mb_strtoupper($commande["Nom_Uti"]);
-                        $Prenom_Client = $commande["Prenom_Uti"];
-                        $Id_Statut = $commande["Id_Statut"];
-                        $Id_Commande = $commande["Id_Commande"];
-
-                        echo "<div class='commande'>";
-                        echo "<strong>$htmlClient :</strong> $Prenom_Client $Nom_Client<br>";
-                        echo "<strong>$htmlCOMMANDE :</strong> $Desc_Statut<br>";
-
-                        if (!in_array($Id_Statut, [3, 4])) {
-                            echo '<form action="change_status_commande.php" method="post">
-                                    <select name="categorie">
-                                        <option value="">' . $htmlModifierStatut . '</option>';
-                            foreach ($categories as $key => $label) {
-                                if ($key > 0) echo "<option value='$key'>$label</option>";
-                            }
-                            echo '  </select>
-                                    <input type="hidden" name="idCommande" value="' . $Id_Commande . '">
-                                    <button type="submit">' . $htmlConfirmer . '</button>
-                                  </form>';
-                        }
-
-                        $total = 0;
-                        $produitsCommande = getProduitsCommande($bdd, $Id_Commande);
-                        if (!empty($produitsCommande)) {
-                            foreach ($produitsCommande as $produit) {
-                                $prixTotal = intval($produit["Prix_Produit_Unitaire"]) * intval($produit["Qte_Produit_Commande"]);
-                                $total += $prixTotal;
-                                echo "- {$produit['Nom_Produit']} ({$produit['Qte_Produit_Commande']} {$produit['Nom_Unite_Prix']} * {$produit['Prix_Produit_Unitaire']}€) = $prixTotal €<br>";
-                            }
-                            echo '<input type="button" onclick="window.location.href=\'ViewMessagerie.php?Id_Interlocuteur=' . $commande["Id_Uti"] . '\'" value="' . $htmlEnvoyerMessage . '"><br>';
-                        }
-                        echo "<strong style='float:right;'>$htmlTotalDeuxPoints $total€</strong>";
-                        echo "</div><br>";
-                    }
-                }
+                $total = 0;
+                $produitsCommande = getProduitsCommande($bdd, $commande['Id_Commande']);
                 ?>
-            </div>
-        </div>
-    </div>
 
-    <?php require "popups/gestion_popups.php"; ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+                <?php if (!empty($produitsCommande)): ?>
+                    <?php foreach ($produitsCommande as $produit): ?>
+                        <p>
+                            <?php echo "- {$produit['Nom_Produit']} : {$produit['Qte_Produit_Commande']} {$produit['Nom_Unite_Prix']} x {$produit['Prix_Produit_Unitaire']}€ = " .
+                                ($produit['Prix_Produit_Unitaire'] * $produit['Qte_Produit_Commande']) . "€"; ?>
+                        </p>
+                        <?php $total += $produit['Prix_Produit_Unitaire'] * $produit['Qte_Produit_Commande']; ?>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+
+                <strong>Total:</strong> <?php echo $total; ?>€<br>
+            </div>
+        <?php endforeach; ?>
+    <?php endif;
