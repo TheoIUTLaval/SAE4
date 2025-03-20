3fa814a6745fb35cb33b5a45dca8c98927d26f8e
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-20 10:54:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html lang="fr">
 <head>
     <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
     require "language/language.php";
-    require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
