26f327ac91c27e0cdf8a6cd8f1658a8e1ac1eecc
mathilde2801 <mathilde72m@gmail.com>
2025-03-20 08:45:59 +0100

    Add Bootstrap CSS and JS for improved styling and functionality in ViewBroadcastuser.php

--- ViewBroadcastuser.php
+++ ViewBroadcastuser.php
@@ -6,6 +6,7 @@
 ?>
     <title><?php echo $htmlMarque; ?></title>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/template.css">
     <link rel="stylesheet" type="text/css" href="css/style_général.css">
     <link rel="stylesheet" type="text/css" href="css/popup.css">
@@ -28,12 +29,12 @@
                     <label for="message" class="form-label"><?php echo $htmlVotreMessage; ?></label>
                     <textarea id="message" name="message" rows="5" maxlength="5000" class="form-control" required></textarea>
                 </div>
-
+                
                 <div class="mb-3">
                     <button type="submit" class="btn btn-primary"><?php echo $htmlEnvoyerMessageATousUti; ?></button>
                 </div>
             </form>
-            <h2>Fuck</h2>
+            
 
             </div>
             <div class="basDePage">
@@ -49,4 +50,5 @@
         </div>
     </div>
     <?php require "popups/gestion_popups.php";?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
