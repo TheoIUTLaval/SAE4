cf1a0df780f86fd14e1af3d3a3447d84768362d9
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-20 11:09:37 +0100

    Update chargement_info_perso.php

--- traitements/chargement_info_perso.php
+++ traitements/chargement_info_perso.php
@@ -10,10 +10,10 @@ $bdd=dbConnect();
 // Préparez la requête SQL en utilisant des requêtes préparées pour des raisons de sécurité
 $requete = 'SELECT * FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti=?';
 $stmt = $bdd->prepare($requete);
-$stmt->bind_param("s", $_SESSION['Mail_Uti']); // "s" indique que la valeur est une chaîne de caractères
+$stmt->bindValue(1, $_SESSION['Mail_Uti'], PDO::PARAM_STR); // "1" indique le premier paramètre, PDO::PARAM_STR indique une chaîne de caractères
 $stmt->execute();
 $result = $stmt->get_result();
 
 $stmt->close();
-$connexion->close();
+$bdd = null;
 ?>
