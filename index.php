14dbfddc494c9611f74f6e15936055f36aad14a0
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-20 16:09:37 +0100

    essaye3

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     require "language/language.php";
+    require "popups/gestion_popups.php";
+    
     require __DIR__ . '/vendor/autoload.php';
 
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
@@ -504,9 +507,5 @@ function distance($lat1, $lng1, $lat2, $lng2, $miles = false)
 </div>
 <?php require "popups/gestion_popups.php"; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script>
-    function confirmDeleteAccount() {
-        return confirm("Are you sure you want to delete your account?");
-    }
-</script>
+
 </body>
