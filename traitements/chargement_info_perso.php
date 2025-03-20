0a9f1181785492ab868c64aef036ca77aceb259c
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-20 11:06:57 +0100

    Update chargement_info_perso.php

--- traitements/chargement_info_perso.php
+++ traitements/chargement_info_perso.php
@@ -1,19 +1,40 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
-
-// Vérifiez la connexion
-if ($connexion->connect_error) {
-    die("Erreur de connexion : " . $connexion->connect_error);  
+function dbConnect(){
+    $utilisateur = $_ENV['DB_USER'];
+    $serveur = $_ENV['DB_HOST'];
+    $motdepasse = $_ENV['DB_PASSWORD'];
+    $basededonnees = $_ENV['DB_NAME'];
+    // Connect to database
+    return new PDO('mysql:host=' . $serveur . ';dbname=' . $basededonnees, $utilisateur, $motdepasse, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
 }
-// Préparez la requête SQL en utilisant des requêtes préparées pour des raisons de sécurité
-$requete = 'SELECT * FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti=?';
-$stmt = $connexion->prepare($requete);
-$stmt->bind_param("s", $_SESSION['Mail_Uti']); // "s" indique que la valeur est une chaîne de caractères
-$stmt->execute();
-$result = $stmt->get_result();
 
-$stmt->close();
-$connexion->close();
+$bdd = dbConnect();
+
+try {
+    // Préparez la requête SQL en utilisant des requêtes préparées pour des raisons de sécurité
+    $requete = 'SELECT * FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti = :mail';
+    $stmt = $bdd->prepare($requete);
+    $stmt->bindParam(':mail', $_SESSION['Mail_Uti'], PDO::PARAM_STR);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+
+    // Traitement des résultats
+    foreach ($result as $row) {
+        // Faites quelque chose avec chaque ligne de résultat
+    }
+
+    $stmt->closeCursor();
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+
+$bdd = null;
 ?>
