dc88e23ad4c44845159038f9e5fa927d062b12ff
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-20 11:11:59 +0100

    Update chargement_info_perso.php

--- traitements/chargement_info_perso.php
+++ traitements/chargement_info_perso.php
@@ -3,14 +3,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
-function dbConnect(){
-    $utilisateur = $_ENV['DB_USER'];
-    $serveur = $_ENV['DB_HOST'];
-    $motdepasse = $_ENV['DB_PASSWORD'];
-    $basededonnees = $_ENV['DB_NAME'];
-    // Connect to database
-    return new PDO('mysql:host=' . $serveur . ';dbname=' . $basededonnees, $utilisateur, $motdepasse);
-}
+
 $connexion=dbConnect();
 
 // Vérifiez la connexion
