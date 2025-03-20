965965895d80ee85e846f935edea886a47cc5721
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-20 13:59:35 +0100

    Update ViewMessagerie.php

--- ViewMessagerie.php
+++ ViewMessagerie.php
@@ -149,10 +149,7 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    
-    <div class="basDePage">
+            <div class="basDePage">
         
             
         <div class="btn-group" role="group" aria-label="Basic outlined example" >
@@ -180,6 +177,10 @@
                     </div>
                 </div>
         </div>
+        </div>
+    </div>
+    
+    
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
