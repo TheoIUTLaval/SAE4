80df234fa945c2f7c9043ec516c4d251fc72f820
TheoIUTLaval <153519636+TheoIUTLaval@users.noreply.github.com>
2025-03-20 10:33:01 +0100

    Update modeleAchat.php

--- modele/modeleAchat.php
+++ modele/modeleAchat.php
@@ -11,7 +11,7 @@
     function dbConnect(){
         $utilisateur = $_ENV['DB_USER'];
         $serveur = $_ENV['DB_HOST'];
-        $motdepasse = $_ENV['DB_PASS'];
+        $motdepasse = $_ENV['DB_PASSWORD'];
         $basededonnees = $_ENV['DB_NAME'];
         // Connect to database
         return new PDO('mysql:host=' . $serveur . ';dbname=' . $basededonnees, $utilisateur, $motdepasse);
